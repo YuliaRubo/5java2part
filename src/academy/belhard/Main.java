98c5a2b71d905c8eea9705533305bb558dcb6837
Ulya Ivancova <ulya-rubo@mail.ru>
2021-04-12 12:17:19 +0200

    commit12

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -14,6 +14,6 @@ public class Main {
 
 
         System.out.println(man2.getFullInfo());
-
+        System.out.println(man2.isPasswordCorrect("hfh"));
     }
 }
--- src/academy/belhard/entity/User.java
+++ src/academy/belhard/entity/User.java
@@ -1,23 +1,36 @@
 package academy.belhard.entity;
 
-public class User extends Person{
+public class User extends Person {
     private String email;
     private String password;
 
-    public User(String email,String password, String firstname, String lastname){
+
+
+
+
+    public User(String email, String password, String firstname, String lastname) {
         super(firstname, lastname);
-        this.email=email;
+        this.email = email;
         this.password = password;
     }
+
     public String getFullInfo() {
-        final String s = "Имя :" + getFullName()+"\n" + "Email :" + email;
+        final String s = "Имя :" + getFullName() + "\n" + "Email :" + email;
         return s;
     }
-    public boolean isPasswordCorrect(String password){
-        if(this.password==password);{
+//    public boolean equals(String password);{
+//        return this.password==password;
+//    }
+
+    public  boolean isPasswordCorrect(String password) {
+        if (this.password.equals(password)) {
             return true;
+
+        } else {
+            return false;
         }
     }
+
 }
 
 
